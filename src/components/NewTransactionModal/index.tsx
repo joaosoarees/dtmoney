4b9b7137e77e4ebc4441e4489e37c50ff5ce1ac9
João Soares <jsoareez@gmail.com>
2021-05-22 20:53:03 -0300

    configure miragejs to work as a backend

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
+import { api } from '../../services/api';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -22,12 +23,14 @@ export function NewTransactionModal({
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    console.log({
+    const data = {
       title,
       value,
       category,
       type,
-    });
+    };
+
+    api.post('transactions', data);
   }
 
   return (
