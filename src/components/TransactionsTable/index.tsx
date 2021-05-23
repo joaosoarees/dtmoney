75305eb82dc7135f8839042e5ef1a6821758db31
João Soares <jsoareez@gmail.com>
2021-05-22 22:27:21 -0300

    summary dynamic with context api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -22,10 +22,19 @@ export function TransactionsTable() {
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
-                {new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                }).format(transaction.amount)}
+                {transaction.type === 'withdraw'
+                  ? new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                      currencySign: 'accounting',
+                      signDisplay: 'always',
+                    }).format(-Math.abs(transaction.amount))
+                  : new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                      currencySign: 'accounting',
+                      signDisplay: 'always',
+                    }).format(transaction.amount)}
               </td>
               <td>{transaction.category}</td>
               <td>
